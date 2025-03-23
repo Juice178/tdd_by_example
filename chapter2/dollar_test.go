4226d73c4251e523e9beb56106b4516b3b042850
kensuz14 <kentasuzuki5555@gmail.com>
2025-03-23 17:07:24 +0900

    feat: Remove side effects

---
+++ chapter2/dollar_test.go
@@ -0,0 +1,16 @@
+package money
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMultiplication(t *testing.T) {
+	assert := assert.New(t)
+	five := Dollar{5}
+	product := five.Times(2)
+	assert.Equal(10, product.amount)
+	product = five.Times(3)
+	assert.Equal(15, product.amount)
+}
